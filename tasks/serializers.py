15185fde50a875153bef29a3a69803debc5d9c3c
anmol choudhary <78426956+Anmol-choudhary-spec@users.noreply.github.com>
2022-03-05 11:57:07 +0530

    initial commit 2

---
+++ tasks/serializers.py
@@ -0,0 +1,24 @@
+from .models import Post, Comment
+from rest_framework import serializers
+from .models import NormalTasks, SpecialTasks
+
+
+
+class NormalSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the comment objects
+    """
+    class Meta:
+        model = NormalTasks
+        fields = '__all__'
+    
+
+class SpecialSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the comment objects
+    """
+    class Meta:
+        model = SpecialTasks
+        fields = '__all__'
+    
+
