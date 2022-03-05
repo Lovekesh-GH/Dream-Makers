2ecce96e23d3d746a8663cd6bd25d0e03d76a9b4
anmol choudhary <78426956+Anmol-choudhary-spec@users.noreply.github.com>
2022-03-05 16:53:30 +0530

    store

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,15 +1,16 @@
 
 from django.urls import path
+from tasks import views
 
 from tasks.views import  taskView, taskViewUpdate, StaskView, StaskViewUpdate, CompletedView, CompletedUpdate, SCompletedView, SCompletedViewUpdate
 urlpatterns = [
-    path("", taskView.as_view()),
-    path("<str:pk>/", taskViewUpdate.as_view()),
-    path("special/", StaskView.as_view()),
-    path("special/<str:pk>/", StaskViewUpdate.as_view()),
-    path("completed/", CompletedView.as_view()),
-    path("completed/<str:pk>/", CompletedUpdate.as_view()),
-    path("SCompleted/", SCompletedView.as_view()),
-    path("sCompleted/<str:pk>/", SCompletedViewUpdate.as_view()),
+    path("", views.taskView.as_view()),
+    path("<str:pk>/", views.taskViewUpdate.as_view()),
+    path('special/', views.StaskView.as_view(),  name='add-comment'),
+    path("special/<str:pk>/", views.StaskViewUpdate.as_view()),
+    path("completed/", views.CompletedView.as_view()),
+    path("completed/<str:pk>/", views.CompletedUpdate.as_view()),
+    path("SCompleted/", views.SCompletedView.as_view()),
+    path("sCompleted/<str:pk>/", views.SCompletedViewUpdate.as_view()),
     
 ]
